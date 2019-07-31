9c1780a47f6dd308e960f01b678c8ac274782142
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-07-31 14:24:21 -0400

    A function that takes a name and returns a greeting challenge

---
+++ Challenges/Hello_name.py
@@ -0,0 +1,11 @@
+# Author: Tobias Scott
+# Website:  https://tobiascscott.com/
+# https://www.hackerrank.com/t_cscott2023
+# https://edabit.com/user/hQGgHwFhPE6fCkn8P
+
+# Challenge: Create a function that takes a name and returns a greeting.
+
+def hello_name(name):
+    return  f"Hello {name}!"
+
+#Sweet and Simple
--- Challenges/Smallest_num_in_List.py
+++ Challenges/Smallest_num_in_List.py
@@ -6,9 +6,9 @@
 # Challenge: Create a function that takes a list of numbers and returns the smallest number in the list.
 
 def findSmallestNum(my_list):
-    entire_list = my_list[0]
+    min = my_list[0]
     for i in my_list:
-        if i < entire_list:
-            entire_list = i
-    return entire_list
+        if i < min:
+            min = i
+    return min
         
