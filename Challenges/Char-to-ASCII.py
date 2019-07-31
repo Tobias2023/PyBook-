8f548b6beb1af2ef1acd63cd78c6e2d46e7a1416
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-07-31 14:42:49 -0400

    a function that returns the ASCII value of the passed in character

---
+++ Challenges/Char-to-ASCII.py
@@ -0,0 +1,10 @@
+# Author: Tobias Scott
+# Website:  https://tobiascscott.com/
+# https://www.hackerrank.com/t_cscott2023
+# https://edabit.com/user/hQGgHwFhPE6fCkn8P
+
+# Challenge: Create a function that returns the ASCII value of the passed in character.
+
+def ctoa(char):
+    print(ord(char))
+    return;
