80148de60a960490be74b923d6442d6bb1709643
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-08-01 08:45:11 -0400

    Create a function that returns True if a string is empty and False otherwise.

---
+++ Challenges/Is_the_String_Empty.py
@@ -0,0 +1,10 @@
+# Author: Tobias Scott
+# Website:  https://tobiascscott.com/
+# https://www.hackerrank.com/t_cscott2023
+# https://edabit.com/user/hQGgHwFhPE6fCkn8P
+
+# Challenge: Create a function that returns True if a string is empty and False otherwise.
+
+def is_empty(s):
+    empty_string = (s == "")
+    return empty_string
