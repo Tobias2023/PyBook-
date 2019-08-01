f6e03254258959919b3bea2689e7cfb089697129
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-08-01 08:33:55 -0400

    Create a function that returns True if an integer is divisible by 5, and false otherwise.

---
+++ Challenges/divisible_by_five.py
@@ -0,0 +1,14 @@
+# Author: Tobias Scott
+# Website:  https://tobiascscott.com/
+# https://www.hackerrank.com/t_cscott2023
+# https://edabit.com/user/hQGgHwFhPE6fCkn8P
+
+# Challenge: Create a function that returns True if an integer is divisible by 5, and false otherwise.
+
+def divisible_by_five(n):
+    divisible = n % 5 == 0
+    return divisible
+    
+    
+    
+    
