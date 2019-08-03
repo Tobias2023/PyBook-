0cf60e9792cfedebbf88b6653a5fc631d69f6d46
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-08-03 19:49:39 -0400

    Given two strings, first_name and last_name, return a single string in the format: last, first.

---
+++ Challenges/Concatenate_First_and_Last_Name_into_One_String.py
@@ -0,0 +1,12 @@
+# Author: Tobias Scott
+# Website:  https://tobiascscott.com/
+# https://www.hackerrank.com/t_cscott2023
+# https://edabit.com/user/hQGgHwFhPE6fCkn8P
+
+# Challenge: Given two strings, first_name and last_name, return a single string in the format "last, first".
+
+def concat_name(first_name, last_name):
+    full_name = (last_name + ", " + first_name)
+    return full_name
+        
+        
