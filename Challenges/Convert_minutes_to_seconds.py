afdb2d39722735ebe0defcfa482760500c27e2de
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-11-18 16:21:18 -0500

    Function to convert minutes to seconds

---
+++ Challenges/Convert_minutes_to_seconds.py
@@ -0,0 +1,10 @@
+# Author: Tobias Scott
+# Website:  https://tobiascscott.com/
+# https://www.hackerrank.com/t_cscott2023
+# https://edabit.com/challenge/uPtuNNTuASzPZMQrW
+
+# Write a function that takes an integer minutes and converts it to seconds.
+
+def convert(minutes):
+	result = minutes * 60
+	return result
