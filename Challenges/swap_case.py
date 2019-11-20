2d79bcd7e8aa4918889740b9b7c99b51cedb0e78
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-11-19 20:27:23 -0500

    Add Swap_case challenge

---
+++ Challenges/swap_case.py
@@ -0,0 +1,8 @@
+# Author: Tobias Scott
+# Website:  https://tobiascscott.com/
+# https://www.hackerrank.com/t_cscott2023
+
+#(Hackerrank)You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+def swap_case(s):
+    return s.swapcase()
