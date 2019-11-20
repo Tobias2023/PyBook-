c4eb83a159a814a4765c003352c43a964b4e4fd7
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-11-19 20:28:40 -0500

    Updated description

--- Challenges/Return-last-list-element.py
+++ Challenges/Return-last-list-element.py
@@ -3,8 +3,7 @@
 # https://www.hackerrank.com/t_cscott2023
 # https://edabit.com/challenge/uPtuNNTuASzPZMQrW
 
-# Challenge: Create a function that returns the ASCII value of the passed in character.
-
+# Challenge: Create a function that returns last item in a list. 
 
 def get_last_item(lst):
 	result = lst[-1]
